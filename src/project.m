d440505967e13cb4bbf003c7babf20f16a4458de
Osman Alperen Kayasaroglu <oakayasaroglu@gmail.com>
2023-01-15 19:42:29 +0300

    project has been added

---
+++ src/project.m
@@ -0,0 +1,441 @@
+function varargout = project(varargin)
+% PROJECT MATLAB code for project.fig
+%      PROJECT, by itself, creates a new PROJECT or raises the existing
+%      singleton*.
+%
+%      H = PROJECT returns the handle to a new PROJECT or the handle to
+%      the existing singleton*.
+%
+%      PROJECT('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in PROJECT.M with the given input arguments.
+%
+%      PROJECT('Property','Value',...) creates a new PROJECT or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before project_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to project_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help project
+
+% Last Modified by GUIDE v2.5 30-May-2022 15:12:14
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @project_OpeningFcn, ...
+                   'gui_OutputFcn',  @project_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before project is made visible.
+function project_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to project (see VARARGIN)
+
+% Choose default command line output for project
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes project wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = project_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in radiobutton1.
+function radiobutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+writeDigitalPin(a,'D2',1);
+writeDigitalPin(a,'D8',1);
+% Hint: get(hObject,'Value') returns toggle state of radiobutton1
+
+
+% --- Executes on button press in radiobutton2.
+function radiobutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+writeDigitalPin(a,'D2',1);
+writeDigitalPin(a,'D9',1);
+% Hint: get(hObject,'Value') returns toggle state of radiobutton2
+
+
+% --- Executes on button press in radiobutton3.
+function radiobutton3_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+writeDigitalPin(a,'D2',1);
+writeDigitalPin(a,'D10',1);
+% Hint: get(hObject,'Value') returns toggle state of radiobutton3
+
+
+% --- Executes on button press in radiobutton4.
+function radiobutton4_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+writeDigitalPin(a,'D2',1);
+writeDigitalPin(a,'D11',1);
+% Hint: get(hObject,'Value') returns toggle state of radiobutton4
+
+
+% --- Executes on button press in radiobutton5.
+function radiobutton5_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton5 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+writeDigitalPin(a,'D2',1);
+writeDigitalPin(a,'D12',1);
+% Hint: get(hObject,'Value') returns toggle state of radiobutton5
+
+
+% --- Executes on button press in radiobutton6.
+function radiobutton6_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton6 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+writeDigitalPin(a,'D2',1);
+writeDigitalPin(a,'D13',1);
+% Hint: get(hObject,'Value') returns toggle state of radiobutton6
+
+
+% --- Executes on button press in radiobutton7.
+function radiobutton7_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton7 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+writeDigitalPin(a,'D3',1);
+writeDigitalPin(a,'D8',1);
+% Hint: get(hObject,'Value') returns toggle state of radiobutton7
+
+
+% --- Executes on button press in radiobutton8.
+function radiobutton8_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton8 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+writeDigitalPin(a,'D3',1);
+writeDigitalPin(a,'D9',1);
+% Hint: get(hObject,'Value') returns toggle state of radiobutton8
+
+
+% --- Executes on button press in radiobutton9.
+function radiobutton9_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton9 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+writeDigitalPin(a,'D3',1);
+writeDigitalPin(a,'D10',1);
+% Hint: get(hObject,'Value') returns toggle state of radiobutton9
+
+
+% --- Executes on button press in radiobutton10.
+function radiobutton10_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton10 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+writeDigitalPin(a,'D3',1);
+writeDigitalPin(a,'D11',1);
+% Hint: get(hObject,'Value') returns toggle state of radiobutton10
+
+
+% --- Executes on button press in radiobutton11.
+function radiobutton11_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton11 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+writeDigitalPin(a,'D3',1);
+writeDigitalPin(a,'D12',1);
+% Hint: get(hObject,'Value') returns toggle state of radiobutton11
+
+
+% --- Executes on button press in radiobutton12.
+function radiobutton12_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton12 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+writeDigitalPin(a,'D3',1);
+writeDigitalPin(a,'D13',1);
+% Hint: get(hObject,'Value') returns toggle state of radiobutton12
+
+
+% --- Executes on button press in radiobutton13.
+function radiobutton13_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton13 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+writeDigitalPin(a,'D4',1);
+writeDigitalPin(a,'D8',1);
+% Hint: get(hObject,'Value') returns toggle state of radiobutton13
+
+
+% --- Executes on button press in radiobutton14.
+function radiobutton14_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton14 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+writeDigitalPin(a,'D4',1);
+writeDigitalPin(a,'D9',1);
+% Hint: get(hObject,'Value') returns toggle state of radiobutton14
+
+
+% --- Executes on button press in radiobutton15.
+function radiobutton15_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton15 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+writeDigitalPin(a,'D4',1);
+writeDigitalPin(a,'D10',1);
+% Hint: get(hObject,'Value') returns toggle state of radiobutton15
+
+
+% --- Executes on button press in radiobutton16.
+function radiobutton16_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton16 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+writeDigitalPin(a,'D4',1);
+writeDigitalPin(a,'D11',1);
+% Hint: get(hObject,'Value') returns toggle state of radiobutton16
+
+
+% --- Executes on button press in radiobutton17.
+function radiobutton17_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton17 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+writeDigitalPin(a,'D4',1);
+writeDigitalPin(a,'D12',1);
+% Hint: get(hObject,'Value') returns toggle state of radiobutton17
+
+
+% --- Executes on button press in radiobutton18.
+function radiobutton18_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton18 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+writeDigitalPin(a,'D4',1);
+writeDigitalPin(a,'D13',1);
+% Hint: get(hObject,'Value') returns toggle state of radiobutton18
+
+
+% --- Executes on button press in radiobutton19.
+function radiobutton19_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton19 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+writeDigitalPin(a,'D5',1);
+writeDigitalPin(a,'D8',1);
+% Hint: get(hObject,'Value') returns toggle state of radiobutton19
+
+
+% --- Executes on button press in radiobutton20.
+function radiobutton20_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton20 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+writeDigitalPin(a,'D5',1);
+writeDigitalPin(a,'D9',1);
+% Hint: get(hObject,'Value') returns toggle state of radiobutton20
+
+
+% --- Executes on button press in radiobutton21.
+function radiobutton21_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton21 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+writeDigitalPin(a,'D5',1);
+writeDigitalPin(a,'D10',1);
+% Hint: get(hObject,'Value') returns toggle state of radiobutton21
+
+
+% --- Executes on button press in radiobutton22.
+function radiobutton22_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton22 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+writeDigitalPin(a,'D5',1);
+writeDigitalPin(a,'D11',1);
+% Hint: get(hObject,'Value') returns toggle state of radiobutton22
+
+
+% --- Executes on button press in radiobutton23.
+function radiobutton23_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton23 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+writeDigitalPin(a,'D5',1);
+writeDigitalPin(a,'D12',1);
+% Hint: get(hObject,'Value') returns toggle state of radiobutton23
+
+
+% --- Executes on button press in radiobutton24.
+function radiobutton24_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton24 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+writeDigitalPin(a,'D5',1);
+writeDigitalPin(a,'D13',1);
+% Hint: get(hObject,'Value') returns toggle state of radiobutton24
+
+
+% --- Executes on button press in radiobutton25.
+function radiobutton25_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton25 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+writeDigitalPin(a,'D6',1);
+writeDigitalPin(a,'D8',1);
+% Hint: get(hObject,'Value') returns toggle state of radiobutton25
+
+
+% --- Executes on button press in radiobutton26.
+function radiobutton26_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton26 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+writeDigitalPin(a,'D6',1);
+writeDigitalPin(a,'D9',1);
+% Hint: get(hObject,'Value') returns toggle state of radiobutton26
+
+
+% --- Executes on button press in radiobutton27.
+function radiobutton27_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton27 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+writeDigitalPin(a,'D6',1);
+writeDigitalPin(a,'D10',1);
+% Hint: get(hObject,'Value') returns toggle state of radiobutton27
+
+
+% --- Executes on button press in radiobutton28.
+function radiobutton28_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton28 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+writeDigitalPin(a,'D6',1);
+writeDigitalPin(a,'D11',1);
+% Hint: get(hObject,'Value') returns toggle state of radiobutton28
+
+
+% --- Executes on button press in radiobutton29.
+function radiobutton29_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton29 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+writeDigitalPin(a,'D6',1);
+writeDigitalPin(a,'D12',1);
+% Hint: get(hObject,'Value') returns toggle state of radiobutton29
+
+
+% --- Executes on button press in radiobutton30.
+function radiobutton30_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton30 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+writeDigitalPin(a,'D6',1);
+writeDigitalPin(a,'D13',1);
+% Hint: get(hObject,'Value') returns toggle state of radiobutton30
+
+
+% --- Executes on button press in radiobutton31.
+function radiobutton31_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton31 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+writeDigitalPin(a,'D7',1);
+writeDigitalPin(a,'D8',1);
+% Hint: get(hObject,'Value') returns toggle state of radiobutton31
+
+
+% --- Executes on button press in radiobutton32.
+function radiobutton32_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton32 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+writeDigitalPin(a,'D7',1);
+writeDigitalPin(a,'D9',1);
+% Hint: get(hObject,'Value') returns toggle state of radiobutton32
+
+
+% --- Executes on button press in radiobutton33.
+function radiobutton33_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton33 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+writeDigitalPin(a,'D7',1);
+writeDigitalPin(a,'D10',1);
+% Hint: get(hObject,'Value') returns toggle state of radiobutton33
+
+
+% --- Executes on button press in radiobutton34.
+function radiobutton34_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton34 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+writeDigitalPin(a,'D7',1);
+writeDigitalPin(a,'D11',1);
+% Hint: get(hObject,'Value') returns toggle state of radiobutton34
+
+
+% --- Executes on button press in radiobutton35.
+function radiobutton35_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton35 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+writeDigitalPin(a,'D7',1);
+writeDigitalPin(a,'D12',1);
+% Hint: get(hObject,'Value') returns toggle state of radiobutton35
+
+
+% --- Executes on button press in radiobutton36.
+function radiobutton36_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton36 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+writeDigitalPin(a,'D7',1);
+writeDigitalPin(a,'D13',1);
+% Hint: get(hObject,'Value') returns toggle state of radiobutton36
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+a = arduino("com3","uno")
